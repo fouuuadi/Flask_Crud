679992fc7ac5942aa96b4b2a4a47d31c6350bbe0
fouuuadi <fouadlamnaouar18@gmail.com>
2024-07-31 21:26:16 +0200

    feat: add getAll and createFriend queries

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,15 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
 app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+import routes
+
+with app.app_context(): 
+    db.create_all() #création de la base de donnée
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/routes.py
+++ backend/routes.py
@@ -2,3 +2,38 @@ from app import app, db
 from flask import request, jsonify 
 from models import Friend
 
+@app.route("/api/friends",methods=["GET"])
+def get_friends():
+    friends = Friend.query.all() #récupère tout les champs de la table Friend (Model Friend)
+    result = [friend.to_json() for friend in friends] # récupère le dictionnaire pour le parcourir
+    return jsonify(result), 200 
+
+#création d'un friend
+@app.route("/api/friends", methods=["POST"])
+def create_friend():
+    try:
+        data = request.json
+        
+        name = data.get("name")
+        role = data.get("role")
+        description = data.get("description")
+        gender = data.get("gender")#genre
+        
+        # Fetch avatar image basee sur ton genre
+        if gender == "male":
+            img_url = f"https://avatar.iran.liara.run/public/boy?username={name}"
+        elif gender == "female":
+            img_url = f"https://avatar.iran.liara.run/public/girl?username={name}"
+        else:
+            img_url = None
+            
+        new_friend = Friend(name=name, role=role, description=description, gender=gender, img_url=img_url)
+            
+        db.session.add(new_friend)    
+        db.session.commit()
+            
+        return jsonify({"msg":"Friend created successfully"}),201
+    
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error":str(e)}),500
